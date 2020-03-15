4c384b539026c3842f4afd27a2da2f523a8ba58f
bryanbuiles <1482@holbertonschool.com>
2020-03-15 12:13:06 -0500

    header and putchar

---
+++ holberton.h
@@ -0,0 +1,31 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct opf
+ * @por: operator porcentaje
+ * @f: pointer to functions.
+ */
+
+typedef struct opf
+{
+	char *por;
+	int (*f)();
+} commands_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+
+
+
+
+
+
+
+
+#endif
